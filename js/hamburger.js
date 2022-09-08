09f47801d2adfb27042ee570059051407ed98bbe
NazariiB <nazikbodna3@gmail.com>
2022-09-08 13:01:17 +0300

    add lab 3

--- js/hamburger.js
+++
@@ -1,7 +0,0 @@
-const humburger = document.querySelector(".humburger");
-const navMenu = document.querySelector(".nav-menu");
-
-humburger.addEventListener("click", () => {
-    humburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
